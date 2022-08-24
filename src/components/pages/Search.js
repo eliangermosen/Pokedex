2e873301be025afcbc483a97bdc93eadd3222729
elianmtg <elianmtoribio@gmail.com>
2022-08-24 09:56:52 -0400

    btns prev and next

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -5,6 +5,16 @@ const Search = () => (
     <section className="container search-cont">
         <SearchBar/>
         <PokeCard/>
+        <div className="btns-next-prev">
+            <a href="/search">
+                <svg className="w-6 h-6" width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
+                <span className="inter-medium">Previous</span>
+            </a>
+            <a href="/search">
+                <span className="inter-medium">Next</span>
+                <svg className="w-6 h-6" width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+            </a>
+        </div>
     </section>
 )
 
