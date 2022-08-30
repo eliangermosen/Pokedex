6b7b0afee3ec787eaa0d6f063274380eaa6a98cc
elianmtg <elianmtoribio@gmail.com>
2022-08-30 13:22:52 -0400

    pokemon view done

--- src/components/sections/Result.js
+++ src/components/sections/Result.js
@@ -8,26 +8,56 @@ const Result = () => (
                 <span className="inter-medium values-rectangle">Electric</span>
                 <span className="inter-medium values-rectangle">Electric</span>
             </div>
-            <div>
+            <div className="physical">
                 <div>
                     <h3 className="inter-bold">Height:</h3>
+                    <div>
                     <span className="inter-medium values-rectangle">0.4m</span>
+                    </div>
                 </div>
                 <div>
                     <h3 className="inter-bold">Weight:</h3>
                     <span className="inter-medium values-rectangle">6.0kg</span>
                 </div>
             </div>
-            <div>
+            <div className="group-habilities">
                 <h3 className="inter-bold">Habilities:</h3>
-                <div>
+                <div className="habilities">
                     <span className="inter-medium values-rectangle">Static</span>
                     <span className="inter-medium values-rectangle">lightning Rod</span>
                 </div>
             </div>
             <div>
                 <h3 className="inter-bold">Stats:</h3>
-                <div className="container-circle">
+                <div className="first-row-stats">
+                    <div className="power">
+                        <span className="inter-medium values-round red">35</span>
+                        <h4 className="inter-bold">HP</h4>
+                    </div>
+                    <div className="power">
+                        <span className="inter-medium values-round yellow">55</span>
+                        <h4 className="inter-bold">Attack</h4>
+                    </div>
+                    <div className="power">
+                        <span className="inter-medium values-round red">40</span>
+                        <h4 className="inter-bold">Defense</h4>
+                    </div>
+                </div>
+                <div className="second-row-stats">
+                    <div className="power">
+                        <span className="inter-medium yellow values-round">50</span>
+                        <h4 className="inter-bold">Special Attack</h4>
+                    </div>
+                    <div className="power">
+                        <span className="inter-medium values-round yellow">50</span>
+                        <h4 className="inter-bold">Special Defense</h4>
+                    </div>
+                    <div className="power">
+                        <span className="inter-medium values-round green">90</span>
+                        <h4 className="inter-bold">Velocity</h4>
+                    </div>
+                </div>
+                {/* <div className="container-circle">
                     <span className="inter-medium values-round red">35</span>
                     <span className="inter-medium values-round yellow">55</span>
                     <span className="inter-medium values-round red">40</span>
@@ -46,7 +76,7 @@ const Result = () => (
                     <h4 className="inter-bold">Special Attack</h4>
                     <h4 className="inter-bold">Special Defense</h4>
                     <h4 className="inter-bold">Velocity</h4>
-                </div>
+                </div> */}
                 {/* <span className="inter-medium values-round red">35</span>
                 <h4 className="inter-bold">HP</h4>
                 <span className="inter-medium values-round yellow">55</span>
