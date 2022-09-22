b4d613828bc9f6335cccee8124515b185cc6237c
elianmtg <elianmtoribio@gmail.com>
2022-09-22 17:36:39 -0400

    first letter of pokemon card to uppercase

--- src/components/cards/pokemonCard.js
+++ src/components/cards/pokemonCard.js
@@ -1,6 +1,12 @@
 // import pikachu from "../../assets/images/pikachu-card.svg"
 
 const pokemonCard = ({number,name,avatar}) => {
+
+    const capitalizeFirstLetter = (name) => {
+        // converting first letter to uppercase
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    };
+
     return(
         // <section className="container-card">
             <article className="content-card">
@@ -8,7 +14,7 @@ const pokemonCard = ({number,name,avatar}) => {
                     <img src={avatar} alt={name} className="img-card" />
                 </figure>
                 <div className="inf-card">
-                    <h2 className="inter-bold name-card">{name}</h2>
+                    <h2 className="inter-bold name-card">{capitalizeFirstLetter(name)}</h2>
                     <h3 className="inter-medium number-card">#{number}</h3>
                 </div>
             </article>
