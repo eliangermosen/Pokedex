ba9a74cd64fd19a9a16d2e678aa1f91e684b2c68
elianmtg <elianmtoribio@gmail.com>
2022-10-27 23:11:04 -0400

    link to github repos

--- src/components/sections/GitHub.js
+++ src/components/sections/GitHub.js
@@ -2,7 +2,7 @@ import gitHub from "../../assets/images/github.svg"
 
 const GitHub = () => (
     <div className="container-git">
-        <a href="https://github.com/Elianmtg/" target="_blank" rel="noreferrer">
+        <a href="https://github.com/Elianmtg/Pokedex" target="_blank" rel="noreferrer">
             <img src={gitHub} alt="Icono de GitHub"/>
         </a>
     </div>
