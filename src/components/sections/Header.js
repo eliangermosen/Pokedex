20e085dfd753e6f690943f2a7323523b28a72804
elianmtg <elianmtoribio@gmail.com>
2022-10-27 21:52:33 -0400

    add meta tags

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -3,7 +3,7 @@ import logo from "../../assets/images/logo.svg"
 const Header = () => (
     <header className = "backg-color">
         <a href="/" className="content-logo">
-            <img src={logo} alt="Logo TvShow"/>
+            <img src={logo} alt="Logo Pokemon"/>
         </a>
     </header>
 )
